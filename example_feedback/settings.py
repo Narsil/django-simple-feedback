6b3f51a9395f81d087729d5455c31929347bba19
Nicolas Patry <nicolas@kwyk.fr>
2011-11-16 17:40:13 +0100

    Updated settings

--- example_feedback/settings.py
+++ example_feedback/settings.py
@@ -136,7 +136,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.messages',
+    #'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
